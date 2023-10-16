fcee56563b20feff1bae9e256f3086843c4825cd
Maxime <144139732+Labatavia@users.noreply.github.com>
2023-10-16 10:48:30 +0200

    quete 2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import PokemonCard from "./components/PokemonCard"
+
+function App() {
+  return(
+ <div>
+  <PokemonCard />
+ </div>
+ );
+}
+
+export default App
